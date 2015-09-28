2e2932cd697ed99272f3a46e6c925eb049e0d2a6
wijan <wiji180892@gmail.com>
2015-09-28 15:19:50 +0200

    10 numeros i més gran

--- mesPetit.java
+++ mesPetit.java
@@ -5,17 +5,22 @@ public class numerosPetit{
 	public static void main(String[] args){
 		Scanner llegir=new Scanner(System.in);
 		int nPetit;
-		int[]numeros=new int[5];
+		int[]numeros=new int[10];
 		for(int i=0;i<numeros.length;i++){
 			System.out.println("Introdueix un numero enter: ");
 			numeros[i]=llegir.nextInt();
 		}
 		nPetit=numeros[0];
+		nGran=numeros[0]
 		for(int numero:numeros){
 			if(numero<nPetit){
 				nPetit=numero;
 			}
+			if(numero>nGran){
+				nGran=numero;
+			}
 		}
 		System.out.println("El més petit és: "+nPetit);
+		System.out.println("El més gran és: "+nGran);
 	}
 }
